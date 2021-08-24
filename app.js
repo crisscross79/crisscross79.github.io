b52a179a9dada6b5602e191af36347826c5dc9f1
crisscross79 <80518902+crisscross79@users.noreply.github.com>
2021-08-24 09:14:37 +0200

    add express and app to app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const http = require('http')
 const fs = require('fs')
 const myfirstmodule = require('./module.js')
+const express = require('express')
+const app = express();
+
+app.get('/contact.html', (req,res) => {
+    res.send('cosik')
+})
 
 const server = http.createServer((req,res) => {
     // res.end('Moja pierwsza aplikacja na node!')
